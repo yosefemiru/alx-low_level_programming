b5917778450657033a7054d40e4306a48db50725
root <Yosef Emiru>
2022-09-10 16:01:45 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -18,18 +18,18 @@ int main(void)
 {
 
 	int d;
+
 	for (d = '0'; d <= '9'; d++)
 	
-	{	
+	{
 		putchar(d);
 		if (d != '9')
 		{
-			putchar(d);
+			putchar(',');
 			putchar(' ');
 		}
 	}
 	putchar('\n');
-
 	return (0); 
 
 }
