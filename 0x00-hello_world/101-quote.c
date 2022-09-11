e337b7eebc98011a869c5630c4c07635e6f458d9
root <Yosef Emiru>
2022-09-11 15:40:15 -0700

    add advance task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,7 +9,8 @@
 
 int main(void)
 {
-	char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(2, ch, sizeof(ch));
+	write(STDOUT_FILENO, "and that piece of art is useful
+
+						\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
